040ced2ae1c90fde4a0822f6253acb8399a5cf35
SalomiMugutmal <mugutmalsalomi@gmail.com>
2024-03-04 18:39:28 +0530

    Initial commit

---
+++ app/src/main/java/com/dhruva/app/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.dhruva.app.Prevalent;
+
+import com.dhruva.app.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
---
+++ app/src/main/java/com/dhruva/app/ViewHolder/ProductViewHolder.java
@@ -0,0 +1,39 @@
+package com.dhruva.app.ViewHolder;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dhruva.app.Interface.ItemClickListner;
+import com.dhruva.app.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView txtProductName, txtProductDescription, txtProductPrice;
+    public ImageView imageView;
+    public ItemClickListner listner;
+    public ProductViewHolder(View itemView)
+    {
+        super(itemView);
+
+
+        imageView = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductDescription = (TextView) itemView.findViewById(R.id.product_description);
+        txtProductPrice = (TextView) itemView.findViewById(R.id.product_price);
+    }
+
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner = listner;
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        listner.onClick(view, getAdapterPosition(), false);
+    }
+}
+
